89d1f0f6adcc9585364efc5afdfa603f8fa69f0e
AtsushiM <at.mizoue@gmail.com>
2012-11-10 22:13:18 +0900

    update

--- js/hyapp.combine.js
+++ js/hyapp.combine.js
@@ -1290,6 +1290,39 @@ Global.Surrogate = Global.klass({
     }
 });
 }());
+/* Test: "../../spec/_src/src/Throttle/test.js" */
+(function() {
+'use strict';
+
+Global.Throttle = Global.klass({
+    constructor: function(config) {
+        this.waittime = config.waittime;
+        this.callback = config.callback;
+        this.locked = false;
+        this.waitid = null;
+    },
+    method: {
+        exec: function(vars) {
+            if (this.locked) {
+                return false;
+            }
+
+            this.callback(vars);
+            this.lock();
+            this.waitid = setTimeout(this.unlock, this.waittime, this);
+        },
+        lock: function() {
+            this.locked = true;
+        },
+        unlock: function(mine) {
+            mine = mine || this;
+
+            mine.locked = false;
+            clearInterval(mine.waitid);
+        }
+    }
+});
+}());
 /* Test: "../../spec/_src/src/Timer/test.js" */
 Global.Timer = function(config) {
     'use strict';
