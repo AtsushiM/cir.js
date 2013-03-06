34637ef1585a90f6acd7e3ecaa33ba07da75aadf
AtsushiM <asionfb@gmail.com>
2013-03-06 23:04:21 +0900

    update

--- js/cir.js
+++ js/cir.js
@@ -1171,36 +1171,44 @@ Tweener.Items = [];
 Tweener['fps'] = 30;
 Tweener['duration'] = 500;
 /* Test: "../../spec/_src/src/selector/test.js" */
-var $base = function(){},
-    checkQuerySelector = /^(.+[\#\.\s\[>:,]|[\[:])/;
+// var $base = function(){},
+//     checkQuerySelector = /^(.+[\#\.\s\[>:,]|[\[:])/;
+var $base = function(){};
+
 C['$'] = function(query, _parent /* varless */, $el, instance, len) {
     // var $el,
     //     base,
     //     instance,
     //     len;
 
+    // if (typeof query == 'string') {
+    //     if (!_parent) {
+    //         if (
+    //             checkQuerySelector.test(query)
+    //         ) {
+    //             $el = doc.querySelectorAll(query);
+    //         }
+    //         else if (query[0] == '#') {
+    //             $el = [doc.getElementById(query.substring(1, query.length))];
+    //         }
+    //         else if (query[0] == '.') {
+    //             $el =
+    //                 doc
+    //                 .getElementsByClassName(query.substring(1, query.length));
+    //         }
+    //         else {
+    //             $el = doc.getElementsByTagName(query);
+    //         }
+    //     }
+    //     else {
+    //         $el = _parent.querySelectorAll(query);
+    //     }
+    // }
+    // else {
+    //     $el = [query];
+    // }
     if (typeof query == 'string') {
-        if (!_parent) {
-            if (
-                checkQuerySelector.test(query)
-            ) {
-                $el = doc.querySelectorAll(query);
-            }
-            else if (query[0] == '#') {
-                $el = [doc.getElementById(query.substring(1, query.length))];
-            }
-            else if (query[0] == '.') {
-                $el =
-                    doc
-                    .getElementsByClassName(query.substring(1, query.length));
-            }
-            else {
-                $el = doc.getElementsByTagName(query);
-            }
-        }
-        else {
-            $el = _parent.querySelectorAll(query);
-        }
+        $el = (_parent || doc).querySelectorAll(query);
     }
     else {
         $el = [query];
--- js/src/selector.js
+++ js/src/selector.js
@@ -1,34 +1,42 @@
 /* Test: "../../spec/_src/src/selector/test.js" */
-var $base = function(){},
-    checkQuerySelector = /^(.+[\#\.\s\[>:,]|[\[:])/;
+// var $base = function(){},
+//     checkQuerySelector = /^(.+[\#\.\s\[>:,]|[\[:])/;
+var $base = function(){};
+
 C['$'] = function(query, _parent /* varless */, $el, instance, len) {
     // var $el,
     //     base,
     //     instance,
     //     len;
 
+    // if (typeof query == 'string') {
+    //     if (!_parent) {
+    //         if (
+    //             checkQuerySelector.test(query)
+    //         ) {
+    //             $el = doc.querySelectorAll(query);
+    //         }
+    //         else if (query[0] == '#') {
+    //             $el = [doc.getElementById(query.substring(1, query.length))];
+    //         }
+    //         else if (query[0] == '.') {
+    //             $el =
+    //                 doc
+    //                 .getElementsByClassName(query.substring(1, query.length));
+    //         }
+    //         else {
+    //             $el = doc.getElementsByTagName(query);
+    //         }
+    //     }
+    //     else {
+    //         $el = _parent.querySelectorAll(query);
+    //     }
+    // }
+    // else {
+    //     $el = [query];
+    // }
     if (typeof query == 'string') {
-        if (!_parent) {
-            if (
-                checkQuerySelector.test(query)
-            ) {
-                $el = doc.querySelectorAll(query);
-            }
-            else if (query[0] == '#') {
-                $el = [doc.getElementById(query.substring(1, query.length))];
-            }
-            else if (query[0] == '.') {
-                $el =
-                    doc
-                    .getElementsByClassName(query.substring(1, query.length));
-            }
-            else {
-                $el = doc.getElementsByTagName(query);
-            }
-        }
-        else {
-            $el = _parent.querySelectorAll(query);
-        }
+        $el = (_parent || doc).querySelectorAll(query);
     }
     else {
         $el = [query];
